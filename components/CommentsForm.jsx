291f18cac0f952b73626b49f72750b80c2090829
RVanathi <vanathisulochana@gmail.com>
2023-04-27 03:21:05 +0530

    Change useRef to useState in commentsForm

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -5,24 +5,44 @@ function CommentsForm({ slug }) {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const commentEl = useRef();
-  const nameEl = useRef();
-  const emailEl = useRef();
-  const storeDataEl = useRef();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    comment: "",
+    storeData: false,
+  });
 
   useEffect(() => {
-    nameEl.current.value = window.localStorage.getItem("name");
-    emailEl.current.value = window.localStorage.getItem("email");
+    setLocalStorage(window.localStorage);
+    const initalFormData = {
+      name: window.localStorage.getItem("name"),
+      email: window.localStorage.getItem("email"),
+      storeData:
+        window.localStorage.getItem("name") ||
+        window.localStorage.getItem("email"),
+    };
+    setFormData(initalFormData);
   }, []);
+
+  const onInputChange = (e) => {
+    const { target } = e;
+    if (target.type === "checkbox") {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }));
+    }
+  };
 
   const handleCommentSubmission = () => {
     setError(false);
-    const { value: comment } = commentEl.current;
-    const { value: name } = nameEl.current;
-    const { value: email } = emailEl.current;
-    const { checked: storeData } = storeDataEl.current;
-
-    if (!comment || !name || !emailEl) {
+    const { name, email, comment, storeData } = formData;
+    if (!name || !email || !comment) {
       setError(true);
       return;
     }
@@ -34,20 +54,32 @@ function CommentsForm({ slug }) {
     };
 
     if (storeData) {
-      window.localStorage.setItem("name", name);
-      window.localStorage.setItem("email", email);
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
     } else {
-      window.localStorage.removeItem("name", name);
-      window.localStorage.removeItem("email", email);
+      localStorage.removeItem("name");
+      localStorage.removeItem("email");
     }
 
     submitComment(commentObj).then((res) => {
-      setShowSuccessMessage(true);
-      setTimeout(() => {
-        setShowSuccessMessage(false);
-      }, 3000);
+      if (res.createComment) {
+        if (!storeData) {
+          formData.name = "";
+          formData.email = "";
+        }
+        formData.comment = "";
+        setFormData((prevState) => ({
+          ...prevState,
+          ...formData,
+        }));
+        setShowSuccessMessage(true);
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 3000);
+      }
     });
   };
+
   return (
     <div className="bg-[#fde2e4] shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b border-[#f08080]/40 pb-4">
@@ -55,7 +87,8 @@ function CommentsForm({ slug }) {
       </h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
-          ref={commentEl}
+          value={formData.comment}
+          onChange={onInputChange}
           className="h-40 p-4 outline-none w-full rounded-lg focus:ring-1 focus:ring-[#f08080] text-gray-700"
           placeholder="Comment"
           name="comment"
@@ -65,14 +98,16 @@ function CommentsForm({ slug }) {
       <div className="grid grid-cols-1  lg:grid-cols-2 gap-4 mb-4">
         <input
           type="text"
-          ref={nameEl}
+          value={formData.name}
+          onChange={onInputChange}
           name="name"
           placeholder="Name"
           className="p-4 outline-none w-full rounded-lg focus:ring-1 focus:ring-[#f08080] text-gray-700"
         />
         <input
           type="email"
-          ref={emailEl}
+          value={formData.email}
+          onChange={onInputChange}
           name="email"
           placeholder="Email"
           className="py-2 px-4 outline-none w-full rounded-lg focus:ring-1 focus:ring-[#f08080] text-gray-700"
@@ -81,7 +116,8 @@ function CommentsForm({ slug }) {
       <div className="grid grid-cols-1 gap-4 mb-4">
         <div>
           <input
-            ref={storeDataEl}
+            checked={formData.storeData}
+            onChange={onInputChange}
             type="checkbox"
             id="storeData"
             name="storeData"
